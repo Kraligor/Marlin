14b6d175d70b1c2aab276e0736e668e801d6bf04
Vertabreaker <opyrus@hotmail.com>
2019-12-22 16:16:56 -0500

    Add files via upload

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2624,19 +2624,60 @@
   #define USER_DESC_4 "PID Autotune Bed"
   #define USER_GCODE_4 "M303 U1 E-1 S60 C8\nM500"
 
+  #define USER_DESC_5 "Lin Adv Off"
+  #define USER_GCODE_5 "M900 K0\nM500"
+
+  #define USER_DESC_6 "Lin Adv K0.2"
+  #define USER_GCODE_6 "M900 K0.2\nM500"
+
+  #define USER_DESC_7 "Lin Adv K0.3"
+  #define USER_GCODE_7 "M900 K0.3\nM500"
+
+  #define USER_DESC_8 "Lin Adv K0.4"
+  #define USER_GCODE_8 "M900 K0.4\nM500"
+
+  #define USER_DESC_9 "Lin Adv K0.5"
+  #define USER_GCODE_9 "M900 K0.5\nM500"
+
+  #define USER_DESC_10 "Lin Adv K0.6"
+  #define USER_GCODE_10 "M900 K0.6\nM500"
+
+
+
   #else 
 
   #define USER_DESC_1 "Probe UBL" //Use probe to setup UBL.
   #define USER_GCODE_1 "G28\nG29 P1\nG29 P3\nG29 S\nG29 A\nM500"
 
-  #define USER_DESC_2 "Adjust Point Near" //Adjust nearest mesh point
-  #define USER_GCODE_2 "G29 P4\nM500"
+  #define USER_DESC_2 "Manual UBL" //Use nozzle & paper to setup UBL
+  #define USER_GCODE_2 "G28\nG29 P4 R999\nG29 S\nG29 A\nM500"
 
-  #define USER_DESC_3 "PID Autotune End"
-  #define USER_GCODE_3 "M303 U1 E0 S200 C8\nM500"
+  #define USER_DESC_3 "Adjust Point Near" //Adjust nearest mesh point
+  #define USER_GCODE_3 "G29 P4\nM500"
 
-  #define USER_DESC_4 "PID Autotune Bed"
-  #define USER_GCODE_4 "M303 U1 E-1 S60 C8\nM500"
+  #define USER_DESC_4 "PID Autotune End"
+  #define USER_GCODE_4 "M303 U1 E0 S200 C8\nM500"
+
+  #define USER_DESC_5 "PID Autotune Bed"
+  #define USER_GCODE_5 "M303 U1 E-1 S60 C8\nM500"
+
+  #define USER_DESC_6 "Lin Adv Off"
+  #define USER_GCODE_6 "M900 K0\nM500"
+
+  #define USER_DESC_7 "Lin Adv K0.2"
+  #define USER_GCODE_7 "M900 K0.2\nM500"
+
+  #define USER_DESC_8 "Lin Adv K0.3"
+  #define USER_GCODE_8 "M900 K0.3\nM500"
+
+  #define USER_DESC_9 "Lin Adv K0.4"
+  #define USER_GCODE_9 "M900 K0.4\nM500"
+
+  #define USER_DESC_10 "Lin Adv K0.5"
+  #define USER_GCODE_10 "M900 K0.5\nM500"
+
+  #define USER_DESC_11 "Lin Adv K0.6"
+  #define USER_GCODE_11 "M900 K0.6\nM500"
 
  #endif
 #endif
