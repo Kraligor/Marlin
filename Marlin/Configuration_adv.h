c22893626712b01bf31a6ce796c8d13dd1c21b61
Vertabreaker <opyrus@hotmail.com>
2019-12-21 21:09:02 -0500

    Add files via upload

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2613,7 +2613,7 @@
 
 #if ENABLED (PROBE_MANUALLY)
   #define USER_DESC_1 "Manual UBL" //Use nozzle & paper to setup UBL
-  #define USER_GCODE_1 "G28\nG29 P4 R999\nG29 A\nG29 S\nM500"
+  #define USER_GCODE_1 "G28\nG29 P4 R999\nG29 S\nG29 A\nM500"
 
   #define USER_DESC_2 "Adjust Point Near" //Adjust nearest mesh point
   #define USER_GCODE_2 "G29 P4\nM500"
@@ -2627,7 +2627,7 @@
   #else 
 
   #define USER_DESC_1 "Probe UBL" //Use probe to setup UBL.
-  #define USER_GCODE_1 "G28\nG29 P1\nG29 P3\nG29 A\nG29 S\nM500"
+  #define USER_GCODE_1 "G28\nG29 P1\nG29 P3\nG29 S\nG29 A\nM500"
 
   #define USER_DESC_2 "Adjust Point Near" //Adjust nearest mesh point
   #define USER_GCODE_2 "G29 P4\nM500"
