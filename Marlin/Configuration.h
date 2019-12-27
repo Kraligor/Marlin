c8f577224e6b1e08f92d688a8a71bc8a69112148
Vertabreaker <opyrus@hotmail.com>
2019-12-27 12:12:18 -0500

    Add files via upload

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -949,7 +949,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 30 }
+#define DEFAULT_MAX_FEEDRATE          { 200, 200, 5, 30 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -962,7 +962,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 5000 }
+#define DEFAULT_MAX_ACCELERATION      { 2000, 2000, 100, 4000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -977,7 +977,7 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define ACCELERATION 1000
+#define ACCELERATION 500
 #define DEFAULT_ACCELERATION          ACCELERATION       // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION (ACCELERATION*2)    // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION   ACCELERATION       // X, Y, Z acceleration for travel (non printing) moves
@@ -1012,7 +1012,7 @@
  *   http://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
 #if DISABLED(CLASSIC_JERK)
-  #define JUNCTION_DEVIATION_MM 0.04 // (mm) Distance from real junction edge
+  #define JUNCTION_DEVIATION_MM 0.01 // (mm) Distance from real junction edge
 #endif
 
 /**
