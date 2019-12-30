8bc18c82de5311741c257c09f6969cd00e58513c
Vertabreaker <opyrus@hotmail.com>
2019-12-30 04:42:41 -0500

    Add files via upload

--- Marlin/Start_here.h
+++ Marlin/Start_here.h
@@ -55,7 +55,7 @@
 //Options
 //#define BEDCLIPS              // Enabled if you have bed clips to avoid manual or probe
 #define MULTIPLE_PROBING 3        // Probe # times take the average
-#define Z_PROBE_SPEED_SLOW (4*60) // Probe speed reduce if accuracy is poor
+#define Z_PROBE_SPEED_SLOW 240 // Probe speed reduce if accuracy is poor
 
 //-----------------------------------
 //logic used to reduce setup steps. |
