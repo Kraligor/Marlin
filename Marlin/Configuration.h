589b4fb7458bb4bce730ed2454ef608b7bb35599
vertabreak <opyrus@hotmail.com>
2019-12-30 08:03:39 -0500

    clean up

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -72,7 +72,6 @@
 
 // Author info of this build printed to the host during boot and M115
 #define STRING_CONFIG_H_AUTHOR "(Vertabreaker)" // Who made the changes.
-//#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 #include "Start_here.h"
 
 /**
